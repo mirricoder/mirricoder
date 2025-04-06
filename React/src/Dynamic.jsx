ff41633de2f0bbe8086f151ec3509c63a41b29a3
mirricoder <mirrifavor@gmail.com>
2025-04-06 11:17:23 +0000

    Dynamic

---
+++ React/src/Dynamic.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './styles.css';
+const ListManager=()=>{
+  const [list, setList] = useState([])
+  const [addItem, setaddItem] = useState("");
+  const handleAdditem = () => {
+    
+}
+export default App
